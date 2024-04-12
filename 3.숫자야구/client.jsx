1744f48db8887ec733cb636c2ff07b4abea581d5
kingaser <kingaser@naver.com>
2024-04-12 16:06:55 +0900

    숫자야구 class 완성

--- 3.숫자야구/client.jsx
+++ 3.숫자야구/client.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import NumberBaseball from './NumberBaseball';
+import NumberBaseballClass from './NumberBaseballClass';
 
-ReactDOM.render(<NumberBaseball />, document.querySelector('#root'));
+ReactDOM.render(<NumberBaseballClass />, document.querySelector('#root'));
