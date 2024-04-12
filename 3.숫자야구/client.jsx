8e7f11954df52d6ac0604da4287cb9c8243aaf6a
kingaser <kingaser@naver.com>
2024-04-12 16:57:34 +0900

    렌더링 최적화1

--- 3.숫자야구/client.jsx
+++ 3.숫자야구/client.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import NumberBaseballClass from './NumberBaseballClass';
+import NumberBaseballClass from './RenderTest';
 
 ReactDOM.render(<NumberBaseballClass />, document.querySelector('#root'));
