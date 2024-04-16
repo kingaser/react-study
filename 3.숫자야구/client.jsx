de694dd858a51687483ae5741acee64afbb782ca
dev-kang <kingaser@naver.com>
2024-04-16 23:14:39 +0900

    reset 버튼 만들기

--- 3.숫자야구/client.jsx
+++ 3.숫자야구/client.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import NumberBaseball from './RenderTest';
+import NumberBaseball from './NumberBaseball';
 
 ReactDOM.render(<NumberBaseball />, document.querySelector('#root'));
