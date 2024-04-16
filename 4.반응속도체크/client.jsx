6ec12c1f029c3d1d81816e02e8bdabc2f67ff28f
dev-kang <kingaser@naver.com>
2024-04-16 23:00:38 +0900

    setTimeout, clearTimeout

---
+++ 4.반응속도체크/client.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ResponseCheckClass from './ResponseCheckClass.jsx';
+
+ReactDOM.render(<ResponseCheckClass/>, document.querySelector('#root'));
